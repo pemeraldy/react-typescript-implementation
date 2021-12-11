4098ec2250c443b6f9d68a6c59c46bcd8af8c398
Peter Olu <pemeraldy@gmail.com>
2021-12-11 09:58:47 +0100

    installed styled component package and cteated style.ts file

---
+++ src/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+    display: flex;
+    align-items: flex-start;
+    background-color: #3179ba;
+    height: 100%;
+    width: 100%;
+    padding: 20px;
+`
